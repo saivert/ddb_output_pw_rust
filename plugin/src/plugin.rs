814a1b79576fefc72a6bfd2675e7f3a7eb4c30ed
Nicolai Syvertsen <saivert@saivert.com>
2023-08-04 21:46:50 +0200

    Use requested field from input buffer
    + misc fixes as usual
    + make sure works with recent pipewire-rs

--- plugin/src/plugin.rs
+++ plugin/src/plugin.rs
@@ -1,9 +1,14 @@
 use crate::*;
 
-use std::{thread, cell::Cell};
 use std::rc::Rc;
+use std::{cell::Cell, thread};
 
-use pipewire::{prelude::*, properties, stream, Context, MainLoop, PW_ID_CORE};
+use pipewire::{
+    prelude::*,
+    properties,
+    stream::{self, StreamFlags},
+    Context, MainLoop, PW_ID_CORE,
+};
 
 pub struct OutputPlugin {
     plugin: DB_output_t,
@@ -24,12 +29,8 @@ enum PwThreadMessage {
     Terminate,
     Pause,
     Unpause,
-    SetFmt {
-        format: ddb_waveformat_t,
-    },
-    SetVol {
-        newvol: f32,
-    },
+    SetFmt { format: ddb_waveformat_t },
+    SetVol { newvol: f32 },
 }
 
 impl PlaybackThread {
@@ -53,7 +54,6 @@ impl PlaybackThread {
 }
 
 impl DBPlugin for OutputPlugin {
-
     fn new(plugin: DB_output_t) -> Self {
         Self {
             plugin,
@@ -70,7 +70,9 @@ impl DBPlugin for OutputPlugin {
         pipewire::init();
     }
     fn plugin_stop(&mut self) {
-        unsafe { pipewire::deinit(); }
+        unsafe {
+            pipewire::deinit();
+        }
     }
 }
 
@@ -83,7 +85,6 @@ impl OutputPlugin {
 }
 
 impl DBOutput for OutputPlugin {
-
     fn init(&mut self) -> i32 {
         if self.requested_fmt.is_none() {
             self.requested_fmt = Some(get_default_waveformat());
@@ -163,20 +164,22 @@ impl DBOutput for OutputPlugin {
     #[allow(unused)]
     fn message(&mut self, msgid: u32, ctx: usize, p1: u32, p2: u32) {
         match msgid {
-            DB_EV_VOLUMECHANGED => {
-                self.msgtothread(PwThreadMessage::SetVol { newvol: DeadBeef::volume_get_amp() })
-            },
+            DB_EV_VOLUMECHANGED => self.msgtothread(PwThreadMessage::SetVol {
+                newvol: DeadBeef::volume_get_amp(),
+            }),
             _ => {}
         }
     }
 
     fn enum_soundcards<F>(&self, callback: F)
-    where F: Fn(&str, &str) + 'static  {
+    where
+        F: Fn(&str, &str) + 'static,
+    {
         let mainloop = MainLoop::new().expect("Failed to create mainloop");
         let context = Context::new(&mainloop).expect("Failed to create context");
         let core = context.connect(None).expect("Failed to connect to remote");
         let registry = core.get_registry().expect("Failed to get registry");
-    
+
         // Register a callback to the `global` event on the registry, which notifies of any new global objects
         // appearing on the remote.
         // The callback will only get called as long as we keep the returned listener alive.
@@ -194,12 +197,12 @@ impl DBOutput for OutputPlugin {
                 }
             })
             .register();
-    
+
         let done = Rc::new(std::cell::Cell::new(false));
         let pending = core.sync(0).expect("Error sync");
         let mainloop_clone = mainloop.clone();
         let done_clone = done.clone();
-    
+
         let _core = core
             .add_listener_local()
             .done(move |id, seq| {
@@ -209,7 +212,7 @@ impl DBOutput for OutputPlugin {
                 }
             })
             .register();
-    
+
         while !done.get() {
             mainloop.run();
         }
@@ -252,7 +255,6 @@ fn set_channel_map(channels: u32, audioinfo: &mut libspa_sys::spa_audio_info_raw
         audioinfo.position[12] = libspa_sys::SPA_AUDIO_CHANNEL_TFL;
         audioinfo.position[13] = libspa_sys::SPA_AUDIO_CHANNEL_TFC;
         audioinfo.position[14] = libspa_sys::SPA_AUDIO_CHANNEL_TFR;
-
     }
     if channels >= 18 {
         audioinfo.position[15] = libspa_sys::SPA_AUDIO_CHANNEL_TRL;
@@ -261,9 +263,13 @@ fn set_channel_map(channels: u32, audioinfo: &mut libspa_sys::spa_audio_info_raw
     }
 }
 
-fn create_audio_format_pod(format: u32, channels: u32, rate: u32, buffer: &mut [u8]) -> *mut libspa_sys::spa_pod {
+fn create_audio_format_pod(
+    format: u32,
+    channels: u32,
+    rate: u32,
+    buffer: &mut [u8],
+) -> *mut libspa_sys::spa_pod {
     unsafe {
-
         let mut b: libspa_sys::spa_pod_builder = std::mem::zeroed();
         b.data = buffer.as_mut_ptr() as *mut c_void;
         b.size = buffer.len() as u32;
@@ -276,13 +282,18 @@ fn create_audio_format_pod(format: u32, channels: u32, rate: u32, buffer: &mut [
         };
         set_channel_map(channels, &mut audioinfo);
 
-        libspa_sys::spa_format_audio_raw_build(&mut b as *mut libspa_sys::spa_pod_builder,
+        libspa_sys::spa_format_audio_raw_build(
+            &mut b as *mut libspa_sys::spa_pod_builder,
             libspa_sys::SPA_PARAM_EnumFormat,
-            &mut audioinfo as *mut libspa_sys::spa_audio_info_raw)
+            &mut audioinfo as *mut libspa_sys::spa_audio_info_raw,
+        )
     }
 }
 
-fn pw_thread_main(init_fmt: ddb_waveformat_t, pw_receiver: pipewire::channel::Receiver<PwThreadMessage>) {
+fn pw_thread_main(
+    init_fmt: ddb_waveformat_t,
+    pw_receiver: pipewire::channel::Receiver<PwThreadMessage>,
+) {
     let mainloop = MainLoop::new().expect("Failed to create mainloop");
     let context = Context::new(&mainloop).expect("Context");
     let core = context.connect(None).expect("Core");
@@ -297,6 +308,7 @@ fn pw_thread_main(init_fmt: ddb_waveformat_t, pw_receiver: pipewire::channel::Re
         *pipewire::keys::NODE_NAME => "DeadBeef",
         *pipewire::keys::APP_NAME => "DeadBeef",
         *pipewire::keys::APP_ID => "music.player.deadbeef",
+        "node.latency" => "1200/48000",
     };
 
     let s = format!("1/{}", init_fmt.samplerate);
@@ -307,96 +319,108 @@ fn pw_thread_main(init_fmt: ddb_waveformat_t, pw_receiver: pipewire::channel::Re
     }
 
     let ourdisconnect = Rc::new(Cell::new(false));
-    let buffersize = Rc::new(Cell::new((init_fmt.bps/8 * init_fmt.channels * 25 * init_fmt.samplerate/1000) as usize));
+    let fmt = Rc::new(Cell::new(init_fmt));
 
-    let mut stream: stream::Stream<()> = match pipewire::stream::Stream::new(
-        &core,
-        "deadbeef",
-        props,
-    ) {
+    let stream: stream::Stream = match pipewire::stream::Stream::new(&core, "deadbeef", props) {
         Ok(a) => a,
-        Err(e) => {    
-            DeadBeef::log_detailed(DDB_LOG_LAYER_DEFAULT, format!("Pipewire: Unable to create stream, {}\n", e.to_string()).as_str());
+        Err(e) => {
+            DeadBeef::log_detailed(
+                DDB_LOG_LAYER_DEFAULT,
+                format!("Pipewire: Unable to create stream, {}\n", e.to_string()).as_str(),
+            );
             DeadBeef::sendmessage(DB_EV_STOP, 0, 0, 0);
             return;
         }
     };
-    
-    let _listener = stream.add_local_listener()
-    .state_changed({
-        let ourdisconnect = ourdisconnect.clone();
-        move |old, new| {
-            println!("State changed: {old:?} -> {new:?}");
-            match new {
-                pipewire::stream::StreamState::Error(x) => {
-                    let msg = format!("Pipewire playback error: {x}");
-                    DeadBeef::log_detailed(DDB_LOG_LAYER_DEFAULT, &msg);
-                    DeadBeef::sendmessage(DB_EV_STOP, 0, 0, 0);
-                },
-                pipewire::stream::StreamState::Unconnected => {
-                    if !ourdisconnect.get() {
-                        DeadBeef::log_detailed(DDB_LOG_LAYER_DEFAULT, "Pipewire disconnected.");
+
+    let _listener = stream
+        .add_local_listener::<()>()
+        .state_changed({
+            let ourdisconnect = ourdisconnect.clone();
+            move |old, new| {
+                println!("State changed: {old:?} -> {new:?}");
+                match new {
+                    pipewire::stream::StreamState::Error(x) => {
+                        let msg = format!("Pipewire playback error: {x}");
+                        DeadBeef::log_detailed(DDB_LOG_LAYER_DEFAULT, &msg);
                         DeadBeef::sendmessage(DB_EV_STOP, 0, 0, 0);
                     }
-                },
-                pipewire::stream::StreamState::Connecting => {
-                    ourdisconnect.set(false);
+                    pipewire::stream::StreamState::Unconnected => {
+                        if !ourdisconnect.get() {
+                            DeadBeef::log_detailed(DDB_LOG_LAYER_DEFAULT, "Pipewire disconnected.");
+                            DeadBeef::sendmessage(DB_EV_STOP, 0, 0, 0);
+                        }
+                    }
+                    pipewire::stream::StreamState::Connecting => {
+                        ourdisconnect.set(false);
+                    }
+                    _ => {}
                 }
-                _ => {}
             }
-        }
-    })
-    .process({
-        let buffersize = buffersize.clone();
-        move |stream, _user_data| {
-            match stream.dequeue_buffer() {
-                None => println!("No buffer received"),
-                Some(mut buffer) => {
-                    let datas = buffer.datas_mut();
+        })
+        .process({
+            let fmt = fmt.clone();
+            move |stream, _| {
+                let fmt = fmt.get();
+                match stream.dequeue_buffer() {
+                    None => println!("No buffer received"),
+                    Some(mut buffer) => {
+                        let req = buffer.requested();
+                        let datas = buffer.datas_mut();
 
-                    let maxsize = datas[0].as_raw().maxsize as usize;
-                    if let Some(d) = datas[0].data() {
+                        let maxsize = datas[0].as_raw().maxsize as i32;
+                        if let Some(d) = datas[0].data() {
+                            let stride = fmt.channels * (fmt.bps / 8);
 
-                        let len = buffersize.get().min(maxsize);
+                            let len = if req > 0 {
+                                req as i32 * stride
+                            } else {
+                                let buffersize = 25 * fmt.samplerate / 1000;
+                                buffersize.min(maxsize / stride)
+                            };
 
-                        let bytesread = if DeadBeef::streamer_ok_to_read(-1) > 0 {
-                            DeadBeef::streamer_read(d.as_mut_ptr() as *mut c_void, len)
-                        } else {
-                            0
-                        };
+                            let bytesread = if DeadBeef::streamer_ok_to_read(-1) > 0 {
+                                DeadBeef::streamer_read(d.as_mut_ptr() as *mut c_void, len as usize)
+                            } else {
+                                0
+                            };
 
-                        *datas[0].chunk_mut().size_mut() = bytesread as u32;
-                        *datas[0].chunk_mut().offset_mut() = 0;
-                        *datas[0].chunk_mut().stride_mut() = 1;
+                            if bytesread < len {
+                                d[bytesread as usize..].fill(0);
+                            }
 
-                        if bytesread == 0 {
-                            buffer.queue();
-                            stream.flush(false).expect("flush");
+                            *datas[0].chunk_mut().size_mut() = bytesread as u32;
+                            *datas[0].chunk_mut().offset_mut() = 0;
+                            *datas[0].chunk_mut().stride_mut() = stride;
+                        }
+                    }
+                };
+            }
+        })
+        .control_info(
+            move |id, control_ptr: *const pipewire::sys::pw_stream_control| {
+                if id == libspa_sys::SPA_PROP_channelVolumes {
+                    unsafe {
+                        let control = *control_ptr;
+                        if control.n_values > 0 {
+                            let values = std::slice::from_raw_parts(
+                                control.values,
+                                control.n_values as usize,
+                            );
+                            for v in values.iter() {
+                                if *v != DeadBeef::volume_get_amp() {
+                                    DeadBeef::volume_set_amp(*v);
+                                    break;
+                                }
+                            }
                         }
                     }
                 }
-            };
-        }
-    })
-    .control_info(
-        move |id, control_ptr: *const pipewire::sys::pw_stream_control| {
-        if id == libspa_sys::SPA_PROP_channelVolumes {
-            unsafe {
-                let control = *control_ptr;
-                if control.n_values > 0 {
-                    let values = std::slice::from_raw_parts(control.values, control.n_values as usize);
-                    for v in values.iter() {
-                        if *v != DeadBeef::volume_get_amp() {
-                            DeadBeef::volume_set_amp(*v);
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-    }).register();
+            },
+        )
+        .register();
 
-    let mut buffer = [0;1024];
+    let mut buffer = [0; 1024];
     let fmtpod = {
         let fmt = init_fmt;
         let format = db_format_to_pipewire(fmt);
@@ -409,12 +433,13 @@ fn pw_thread_main(init_fmt: ddb_waveformat_t, pw_receiver: pipewire::channel::Re
     if let Err(e) = stream.connect(
         pipewire::spa::Direction::Output,
         None,
-        stream::StreamFlags::AUTOCONNECT
-            | stream::StreamFlags::MAP_BUFFERS
-            | stream::StreamFlags::RT_PROCESS,
+        StreamFlags::AUTOCONNECT | StreamFlags::MAP_BUFFERS | StreamFlags::RT_PROCESS,
         &mut [fmtpod],
     ) {
-        DeadBeef::log_detailed(DDB_LOG_LAYER_DEFAULT, format!("Pipewire: Unable to connect stream, {}\n", e.to_string()).as_str());
+        DeadBeef::log_detailed(
+            DDB_LOG_LAYER_DEFAULT,
+            format!("Pipewire: Unable to connect stream, {}\n", e.to_string()).as_str(),
+        );
         DeadBeef::sendmessage(DB_EV_STOP, 0, 0, 0);
         return;
     }
@@ -427,54 +452,65 @@ fn pw_thread_main(init_fmt: ddb_waveformat_t, pw_receiver: pipewire::channel::Re
                 PwThreadMessage::Terminate => {
                     ourdisconnect.set(true);
                     mainloop.quit();
-                },
+                }
                 PwThreadMessage::Pause => stream.set_active(false).unwrap(),
                 PwThreadMessage::Unpause => stream.set_active(true).unwrap(),
                 PwThreadMessage::SetFmt { format } => {
                     ourdisconnect.set(true);
-                    stream.set_active(false).unwrap();
+                    // stream.set_active(false).unwrap();
                     if stream.disconnect().is_ok() {
-
                         print!("Set format called with: ");
                         let pwfmt = db_format_to_pipewire(format);
                         let channels = format.channels as u32;
                         let samplerate = format.samplerate as u32;
                         print_pipewire_format(pwfmt, channels, samplerate);
-    
-                        let mut buffer = [0;1024];
-                        let newformatpod: *mut libspa_sys::spa_pod = create_audio_format_pod(pwfmt, channels, samplerate, &mut buffer);
-                        buffersize.set((format.bps/8 * format.channels * 25 * format.samplerate/1000) as usize);
 
-                        if stream.connect(
-                            pipewire::spa::Direction::Output,
-                            None,
-                            stream::StreamFlags::AUTOCONNECT
-                                | stream::StreamFlags::MAP_BUFFERS
-                                | stream::StreamFlags::RT_PROCESS,
-                            &mut [newformatpod],
-                        ).is_err() {
-                            DeadBeef::log_detailed(DDB_LOG_LAYER_DEFAULT, "Pipewire: Unable to connect stream, terminating\n");
+                        let mut buffer = [0; 1024];
+                        let newformatpod: *mut libspa_sys::spa_pod =
+                            create_audio_format_pod(pwfmt, channels, samplerate, &mut buffer);
+                        fmt.set(format);
+
+                        if stream
+                            .connect(
+                                pipewire::spa::Direction::Output,
+                                None,
+                                StreamFlags::AUTOCONNECT
+                                    | StreamFlags::MAP_BUFFERS
+                                    | StreamFlags::RT_PROCESS,
+                                &mut [newformatpod],
+                            )
+                            .is_err()
+                        {
+                            DeadBeef::log_detailed(
+                                DDB_LOG_LAYER_DEFAULT,
+                                "Pipewire: Unable to connect stream, terminating\n",
+                            );
                             DeadBeef::sendmessage(DB_EV_STOP, 0, 0, 0);
                             return;
                         }
 
-                        let s = format!("1/{}", samplerate);
-                        let props = properties!{
-                            "node.rate" => s
+                        let rs = format!("1/{}", samplerate);
+                        let props = properties! {
+                            "node.rate" => rs,
+                            "node.latency" => "1200/48000",
                         };
                         unsafe {
-                            pipewire::sys::pw_stream_update_properties(stream.as_ptr(), props.get_dict_ptr());
+                            pipewire::sys::pw_stream_update_properties(
+                                stream.as_ptr(),
+                                props.get_dict_ptr(),
+                            );
                         }
                     }
-                },
+                }
                 PwThreadMessage::SetVol { newvol } => {
                     let values = [newvol];
-                    stream.set_control(libspa_sys::SPA_PROP_channelVolumes, &values).expect("Unable to set volume");
+                    stream
+                        .set_control(libspa_sys::SPA_PROP_channelVolumes, &values)
+                        .expect("Unable to set volume");
                 }
             };
         }
     });
 
     mainloop.run();
-
 }
